dad3acbb92ef12a75ff88115086c82491d7ff46b
captain-n3m0 <techtubebydebjit@gmail.com>
2023-06-11 00:02:28 +0530

    fixed.

--- src/filesystem/filesystem.h
+++ src/filesystem/filesystem.h
@@ -1,6 +1,6 @@
-#ifndef FILESYSTEM_H
-#define FILESYSTEM_H
-#include "../disk_operations/disk.h"
+#ifndef FILE_H
+#define FILE_H
+
 struct disk* disk;
 typedef void*(*FS_OPEN_FUNCTION)(struct disk* disk, char* filename, char mode);
 typedef int(*FS_RESOLVE_FUNCTION)(struct disk* disk);
@@ -15,6 +15,5 @@ struct filesystem
 void fs_load();
 int fopen(char* filename, char mode);
 void fs_insert_filesystem(struct filesystem* filesystem);
-struct filesystem* fs_resolve_(struct disk* disk);
-
+struct filesystem* fs_resolve(struct disk* disk);
 #endif
